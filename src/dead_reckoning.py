f1383d52aefe71ca0210d6c1124e08abaad09084
jacob <jhultman0@gmail.com>
2019-01-11 00:49:51 -0500

    add pip requirements and refactor plot

--- src/dead_reckoning.py
+++ src/dead_reckoning.py
@@ -1,37 +1,38 @@
 import numpy as np
-from scipy.misc import imread
+from imageio import imread
 from numpy import cos, sin, pi
 
 
-def load_data(lati_longi_fname, yaw_fname):
-    lati_longi = np.loadtxt(lati_longi_fname, delimiter=',')
-    yaw = np.loadtxt(yaw_fname, delimiter=',')
-    lati = lati_longi[:, 0]
-    longi = lati_longi[:, 1]
-    return lati, longi, yaw
+def load_data(lati_longi_fname, yaw_fname, groundtruth_fname):
+    load = lambda f: np.loadtxt(f, delimiter=',')
+    lati_longi = load(lati_longi_fname)
+    yaw = load(yaw_fname)
+    groundtruth = load(groundtruth_fname)
+    lati, longi = lati_longi.T
+    return lati, longi, yaw, groundtruth
 
 
 def gen_xy_from_lati_longi(lati, longi, c0):
     lati_adj = lati - lati[0];
     longi_adj = longi - longi[0];   
 
-    x = -(c0 * cos(lati) / 360) * longi_adj
-    y = (c0 / 360) * lati_adj
+    x = -longi_adj * (c0 * cos(lati) / 360)
+    y = +lati_adj * (c0 / 360)
     return x, y
 
 
 def rotate(x, y, theta):
     c, s = cos(theta), sin(theta)
-    rotation = np.array([[c, -s], [s, c]])
+    rotation = [[c, -s], [s, c]]
     
-    xy = np.vstack((x.transpose(), y.transpose()))
-    xy = np.dot(rotation, xy)
-    return xy[0, :], xy[1, :]
+    xy = np.vstack((x.T, y.T))
+    x, y = np.dot(rotation, xy)
+    return x, y
 
 
 def gen_xy_from_yaw(yaw, dt, v0):
-    psiR = yaw
     psi0 = -pi / 2 + yaw[0]
+    psiR = yaw
 
     dxdt = v0 * sin(psi0 - psiR)
     dydt = v0 * cos(psi0 - psiR)
@@ -41,13 +42,17 @@ def gen_xy_from_yaw(yaw, dt, v0):
     return x, y
 
 
-def shift_and_scale(x1, y1, x2, y2):
-    sx = np.mean(x2) - np.mean(x1)
-    sy = np.mean(y2) - np.mean(y1)
+def shift_and_scale(x0, y0, x1, y1):
+    """
+    Ensure (x0, y0) and (x1, y1) 
+    have same mean and range.
+    """
+    ux = x1.mean() - x0.mean()
+    uy = y1.mean() - y0.mean()
 
-    alpha_x = (np.max(x2) - np.min(x2)) / (np.max(x1) - np.min(x1))
-    alpha_y = (np.max(y2) - np.min(y2)) / (np.max(y1) - np.min(y1))
+    alpha_x = (x1.max() - x1.min()) / (x0.max() - x0.min())
+    alpha_y = (y1.max() - y1.min()) / (y0.max() - y0.min())
 
-    x2 = (x2 - sx) / alpha_x
-    y2 = (y2 - sy) / alpha_y
-    return x2, y2
+    x1 = (x1 - ux) / alpha_x
+    y1 = (y1 - uy) / alpha_y
+    return x1, y1
--- src/demo.py
+++ src/demo.py
@@ -1,3 +1,4 @@
+import os.path as osp
 import numpy as np
 import matplotlib.pyplot as plt
 from dead_reckoning import *
@@ -7,17 +8,20 @@ def configure_axis(axis, title):
     axis.set_xticklabels([])
     axis.set_yticklabels([])
     axis.tick_params(axis='both', which='both', length=0)
-    axis.set_title(title)
     axis.set_xlabel('East')
     axis.set_ylabel('North')
+    axis.set_title(title)
 
 
-def plot_navigation(x1, y1, x2, y2, img):
+def plot_navigation(x0, y0, x1, y1, groundtruth, img):
+    x2, y2 = groundtruth.T
+
     fig, axarr = plt.subplots(nrows=1, ncols=3, figsize=(12, 4))
-    titles = ['GPS tracking', 'Yaw attitude', 'Google Maps']
+    titles = ['GPS', 'Magnetometer', 'Google Maps']
 
-    axarr[0].plot(x1, y1)
-    axarr[1].plot(x2, y2)
+    axarr[0].plot(x0, y0, c='black')
+    axarr[1].plot(x1, y1, c='black')
+    axarr[2].plot(x2, y2, c='orange', linestyle='dashed')
     axarr[2].imshow(img, aspect='auto')
 
     for i in range(3):
@@ -27,23 +31,28 @@ def plot_navigation(x1, y1, x2, y2, img):
     plt.savefig('../images/dead-reckoning.png')
 
 
-def main():
-    c0 = 40.075 * 1e6;  # Circumference of the Earth (m)
-    v0 = 0.97           # Measured walking speed (m/s)
-    dt = 0.02           # Interval between yaw samples (s)
+def demo():
+    circ = 40.075 * 1e6  # Circumference of the Earth (m)
+    vel = 0.97           # Measured walking speed (m/s)
+    dt = 0.02            # Interval between yaw samples (s)
 
-    fnames = ('../data/lati-longi.csv', '../data/yaw.csv')
-    lati, longi, yaw = load_data(*fnames)
+    datadir = osp.join('..', 'data')
+    fnames = ['lati-longi', 'yaw', 'groundtruth']
+    fpaths = [osp.join(datadir, f'{fname}.csv') for fname in fnames]
+    lati, longi, yaw, groundtruth = load_data(*fpaths)
 
-    x1, y1 = gen_xy_from_lati_longi(lati, longi, c0)
-    x1, y1 = rotate(x1, y1, -pi / 9)
+    x0, y0 = gen_xy_from_lati_longi(lati, longi, circ)
+    x0, y0 = rotate(x0, y0, -pi / 9)
 
-    x2, y2 = gen_xy_from_yaw(yaw, dt, v0)
-    x2, y2 = shift_and_scale(x1, y1, x2, y2)
+    x1, y1 = gen_xy_from_yaw(yaw, dt, vel)
+    x1, y1 = shift_and_scale(x0, y0, x1, y1)
 
     img = imread('../data/upenn.png')
-    plot_navigation(x1, y1, x2, y2, img)
+    plot_navigation(x0, y0, x1, y1, groundtruth, img)
 
+
+def main():
+    demo()
 
 if __name__ == '__main__':
     main()
